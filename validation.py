612ded2f4c04893edf5e5b4f665c5f67ecde431e
kaushik-bhattarai <kaushikbhattarai111@gmail.com>
2026-06-04 11:07:32 +0545

    test on single image

--- validation.py
+++ validation.py
@@ -1,7 +1,17 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, solutions
+model = YOLO("/home/kaushik/Nepali_Num_plate/runs/detect/exp_imgsz640_ep80-2/weights/best.pt")
 
-model = YOLO("runs/detect/train/weights/best.pt")
+#metrics = model.val(data="/home/kaushik/Nepali_Num_plate/num_plate_dataset/data.yaml")
 
-metrics = model.val(data="/home/kaushik/Nepali_Num_plate/num_plate_dataset/data.yaml")
+#print(metrics)
 
-print(metrics)
+#test on a single image
+results = model.predict(
+    source="/home/kaushik/Nepali_Num_plate/num_plate_dataset/images/train/2024-03-08_23_10.jpg",
+    conf=0.25,
+    save=True
+)
+
+for r in results:
+    print(r.boxes.xyxy)
+    print(r.boxes.conf)
